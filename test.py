a85dcb3fdc627ac554df69d592fa39ed5ed023a1
Noah <wwdu2010@gmail.com>
2019-11-01 18:15:35 +0800

    添加测试语言包，完善py文件

--- test.py
+++ test.py
@@ -1,9 +1,46 @@
 # coding: UTF-8  #设置编码
-ff = open('./TXT_de.md', 'w')
-with open('./1.txt', 'r') as f:
-    line = f.readlines()
-    for line_list in line:
-        line_new = line_list.replace('\r\n','')
-        line_new = line_new + ';' + '\r\n'
-        print(line_new)
-        ff.write(line_new)
+import os
+
+directory_path = './TestLanguagePackage'
+
+def fetch_file_name(dir_path):
+    array = []
+    for root, dirs, files in os.walk(dir_path):
+        for file in files:
+            if os.path.splitext(file)[1] == '.txt':
+                array.append(os.path.join(file))
+    return array
+
+
+def appendSymbolToNewFile(originFileName, newFileName):
+    ff = open(newFileName, 'w')
+    with open(originFileName, 'r') as f:
+        line = f.readlines()
+        for line_list in line:
+            line_new = line_list.replace('\r\n','')
+            line_new = line_new + ';' + '\r\n'
+            print(line_new)
+            ff.write(line_new)
+
+
+def createSameNameMarkDownFile(filename):
+        """
+        创建日志文件夹和日志文件
+        :param filename:
+        :return:
+        """
+        path = filename[0:filename.rfind("/")]
+        if not os.path.isdir(path):  # 无文件夹时创建
+            os.makedirs(path)
+        if not os.path.isfile(filename):  # 无文件时创建
+            fd = open(filename, mode="w", encoding="utf-8")
+            fd.close()
+        else:
+            pass
+
+file_name_arrays = fetch_file_name(directory_path)
+print(file_name_arrays)
+# for file_name in file_name_arrays:
+#     markdown_file_name = file_name + ".md"
+#     createSameNameMarkDownFile(markdown_file_name)
+#     appendSymbolToNewFile(file_name, markdown_file_name)
